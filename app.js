e76f617015bf970c9acc09f97a91647552125b6f
Morgan <143568735+Morclouds@users.noreply.github.com>
2024-09-06 00:37:45 -0500

    changes

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-var r = 0; // This variable has global scope
-var g = 0;
-var b = 0;
-
+var r = 255; // This variable has global scope
+var g = 255;
+var b = 255;
+console.log (r, g, b );
 
 
 function rgb(elem) {
-  const name = elem.name; //This varible has block scope
+  let name = elem.name; //This varible has block scope
   let val = elem.value;
   window[name] = val;
-  document.getElementById("id1").style.backgroundColor = 'rgb('+r+','+g+','+b+')';
+  document.getElementById("input").style.backgroundColor = 'rgb('+r+','+g+','+b+')';
 }
 
+function randomColor() {
+    var r = Math.floor(Math.random() * 255); //
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    var randomColor = "rgb(" + r + "," + g + "," + b + ")";  
+    console.log(randomColor);
+    document.getElementById("input").style.backgroundColor = randomColor
+  }
+  
+ 
+
 var input = 'Input Box' // This is the string variable of the title of the input box
-console.log (r, g, b );
+
 console.log (input);
